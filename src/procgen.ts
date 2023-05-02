e3d65163d0e3cede5e8e8db3d63d61b0400cfeb4
Matthew King1 <matthew_d_king@outlook.com>
2023-05-02 08:24:52 +0100

    Finised part 4

--- src/procgen.ts
+++ src/procgen.ts
@@ -1,7 +1,7 @@
-import { Entity } from './entity';
-import { FLOOR_TILE, WALL_TILE, Tile } from './tile-types';
-import { GameMap } from './game-map';
+import { FLOOR_TILE, Tile, WALL_TILE } from './tile-types';
 import { Display } from 'rot-js';
+import { Entity } from './entity';
+import { GameMap } from './game-map';
 
 class RectangularRoom {
   tiles: Tile[][];
@@ -55,7 +55,7 @@ export function generateDungeon(
 
   const rooms: RectangularRoom[] = [];
 
-  //generate rooms up to the given parameters and add them to the room array
+  // generate rooms up to the given parameters and add them to the room array
   for (let count = 0; count < maxRooms; count++) {
     const width = generateRandomNumber(minSize, maxSize);
     const height = generateRandomNumber(minSize, maxSize);
@@ -80,7 +80,7 @@ export function generateDungeon(
     const first = rooms[index];
     const second = rooms[index + 1];
 
-    for (let tile of connectRooms(first, second)) {
+    for (const tile of connectRooms(first, second)) {
       dungeon.tiles[tile[1]][tile[0]] = { ...FLOOR_TILE };
     }
   }
@@ -93,18 +93,18 @@ function* connectRooms(
   b: RectangularRoom
 ): Generator<[number, number], void, void> {
   // set the start point of our tunnel at the center of the first room
-  let current = a.center;
+  const current = a.center;
   // set the end point at the center of the second room
   const end = b.center;
 
   // flip a coin to see if we go horizontally first or vertically
-  let horizontal = Math.random() < 0.5;
+  const horizontal = Math.random() < 0.5;
   // set our axisIndex to 0 (x axis) if horizontal or 1 (y axis) if vertical
   let axisIndex = horizontal ? 0 : 1;
 
   // we'll loop until our current is the same as the end point
   while (current[0] !== end[0] || current[1] !== end[1]) {
-    //are we tunneling in the positive or negative direction?
+    // are we tunneling in the positive or negative direction?
 
     // if direction is 0 we have hit the destination in one direction
     const direction = Math.sign(end[axisIndex] - current[axisIndex]);
