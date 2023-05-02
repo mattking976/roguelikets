880227af3338f4d3e807172dc0d1d50d78b2b896
Matthew King1 <matthew_d_king@outlook.com>
2023-05-02 16:53:36 +0100

    finished tutorial 6 and run the linting

--- src/input-handler.ts
+++ src/input-handler.ts
@@ -1,50 +1,62 @@
-import { Engine } from './engine';
-import { Entity } from './entity';
+import { Actor, Entity } from './entity';
 
 export interface Action {
-  perform: (engine: Engine, entity: Entity) => void;
+  perform: (entity: Entity) => void;
 }
 
 export abstract class ActionWithDirection implements Action {
   constructor(public dx: number, public dy: number) {}
 
-  abstract perform(_engine: Engine, _entity: Entity): void;
+  abstract perform(_entity: Entity): void;
 }
 export class MovementAction extends ActionWithDirection {
-  perform(engine: Engine, entity: Entity) {
+  perform(entity: Entity) {
     const destX = entity.x + this.dx;
     const destY = entity.y + this.dy;
 
-    if (!engine.gameMap.isInBounds(destX, destY)) return;
-    if (!engine.gameMap.tiles[destY][destX].walkable) return;
-    if (engine.gameMap.getBlockingEntityAtLocation(destX, destY)) return;
+    if (!window.engine.gameMap.isInBounds(destX, destY)) return;
+    if (!window.engine.gameMap.tiles[destY][destX].walkable) return;
+    if (window.engine.gameMap.getBlockingEntityAtLocation(destX, destY)) return;
     entity.move(this.dx, this.dy);
   }
 }
 
 export class MeleeAction extends ActionWithDirection {
-  perform(engine: Engine, entity: Entity) {
-    const destX = entity.x + this.dx;
-    const destY = entity.y + this.dy;
+  perform(actor: Actor) {
+    const destX = actor.x + this.dx;
+    const destY = actor.y + this.dy;
 
-    const target = engine.gameMap.getBlockingEntityAtLocation(destX, destY);
-
+    const target = window.engine.gameMap.getActorAtLocation(destX, destY);
     if (!target) return;
 
-    console.log(`You kick the ${target.name}, much to its annoyance!`);
+    const damage = actor.fighter.power - target.fighter.defence;
+    const attackDescription = `${actor.name.toUpperCase()} attacks ${target.name}`;
+
+    if (damage > 0) {
+      console.log(`${attackDescription} for ${damage} hit points.`);
+      target.fighter.hp -= damage;
+    } else {
+      console.log(`${attackDescription} but does no damage.`);
+    }
   }
 }
 
 export class BumpAction extends ActionWithDirection {
-  perform(engine: Engine, entity: Entity) {
+  perform(entity: Entity) {
     const destX = entity.x + this.dx;
     const destY = entity.y + this.dy;
 
-    if (engine.gameMap.getBlockingEntityAtLocation(destX, destY)) {
-      return new MeleeAction(this.dx, this.dy).perform(engine, entity);
+    if (window.engine.gameMap.getActorAtLocation(destX, destY)) {
+      return new MeleeAction(this.dx, this.dy).perform(entity as Actor);
     } else {
-      return new MovementAction(this.dx, this.dy).perform(engine, entity);
+      return new MovementAction(this.dx, this.dy).perform(entity);
     }
+  }
+}
+
+export class WaitAction implements Action {
+  perform(_entity: Entity) {
+    // does nothing
   }
 }
 
@@ -53,10 +65,36 @@ interface MovementMap {
 }
 
 const MOVE_KEYS: MovementMap = {
-  ArrowUp: new MovementAction(0, -1),
-  ArrowDown: new MovementAction(0, 1),
-  ArrowLeft: new MovementAction(-1, 0),
-  ArrowRight: new MovementAction(1, 0)
+  // Arrow Keys
+  ArrowUp: new BumpAction(0, -1),
+  ArrowDown: new BumpAction(0, 1),
+  ArrowLeft: new BumpAction(-1, 0),
+  ArrowRight: new BumpAction(1, 0),
+  Home: new BumpAction(-1, -1),
+  End: new BumpAction(-1, 1),
+  PageUp: new BumpAction(1, -1),
+  PageDown: new BumpAction(1, 1),
+  // Numpad Keys
+  1: new BumpAction(-1, 1),
+  2: new BumpAction(0, 1),
+  3: new BumpAction(1, 1),
+  4: new BumpAction(-1, 0),
+  6: new BumpAction(1, 0),
+  7: new BumpAction(-1, -1),
+  8: new BumpAction(0, -1),
+  9: new BumpAction(1, -1),
+  // Vi keys
+  h: new BumpAction(-1, 0),
+  j: new BumpAction(0, 1),
+  k: new BumpAction(0, -1),
+  l: new BumpAction(1, 0),
+  y: new BumpAction(-1, -1),
+  u: new BumpAction(1, -1),
+  b: new BumpAction(-1, 1),
+  n: new BumpAction(1, 1),
+  // Wait keys
+  5: new WaitAction(),
+  Period: new WaitAction()
 };
 
 export function handleInput(event: KeyboardEvent): Action {
